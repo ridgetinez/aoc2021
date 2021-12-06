0a2b6cec832fa13012c967c2951f064de6c0cc74
Adrian Martinez <amartinez@atlassian.com>
2021-12-06 16:56:11 +1100

    dirty way to do day02
    I'd like to create a function that generalises processing over the two days so that I don't have to repeat the pattern matching

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,5 +1,7 @@
 module Day02 where
 
+import Data.Either
+import Data.Bifunctor
 import Text.ParserCombinators.Parsec
 
 commands :: GenParser Char st [(String,Int)]
@@ -10,16 +12,35 @@ commands =
 
 command :: GenParser Char st (String,Int)
 command =
-    do commandId <- many letter 
+    do commandId <- many letter
        char ' '
-       delta     <- many digit
+       delta <- many digit
        many $ char '\n'
        return (commandId, read delta)
 
 parseCommands :: String -> Either ParseError [(String,Int)]
 parseCommands = parse commands "(unknown)"
 
+runCommandsB :: [(String,Int)] -> (Int,Int)
+runCommandsB =  snd . foldl interpretCommand startingConditions
+    where interpretCommand (aim,(x,y)) ("forward",n) = (aim,(x+n,y+aim*n))
+          interpretCommand (aim,(x,y)) ("up",n)      = (aim-n,(x,y))
+          interpretCommand (aim,(x,y)) ("down",n)    = (aim+n,(x,y))
+          startingConditions = (0,(0,0))
+
+runCommandsA :: [(String,Int)] -> (Int,Int)
+runCommandsA =  bimap sum sum . unzip . map interpretCommand
+    where interpretCommand ("forward",n) = (n,0)
+          interpretCommand ("up",n)      = (0,-n)
+          interpretCommand ("down",n)    = (0,n)
+
 day02a :: IO ()
 day02a = do
     input <- readFile "data/day02.txt"
-    putStrLn $ show . parseCommands $ input
+    putStrLn $ show . runCommandsA . fromRight [] . parseCommands $ input
+
+
+day02b :: IO ()
+day02b = do
+    input <- readFile "data/day02.txt"
+    putStrLn $ show . runCommandsB . fromRight [] . parseCommands $ input
